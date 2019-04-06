6f1eb85c7dbccedded2d0d1120c303d7a02a72c5
faiznoeris <m.faiznoeris@gmail.com>
2019-04-07 00:25:37 +0700

    fix

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ bot.on("ready", async () => {
 
     const guild = bot.guilds.get('310446682900791297');
     const totalUsers = bot.channels.get('563975579032879105');
+    const totalDiscordUsers = bot.channels.get('564009370850164737');
 
     setInterval(function() {
         // console.log('Getting stats update..')
         var mcIP = '139.99.70.135';
         var mcPort = 25574;
         var userCount = 0;
+
+        const discordUsers = bot.guild.memberCount;
+        totalDiscordUsers.setName("Total members: " + discordUsers)
+            // .then(newChannel => console.log(`Stat channel renamed to: ${newChannel.name}`))
+            .catch(console.error);
 
         var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
         request(url, function(err, response, body) {
@@ -27,7 +33,7 @@ bot.on("ready", async () => {
             if(body.online) {
                 if(body.players) {
                     userCount = body.players.now;
-                    totalUsers.setName("Total Players: " + userCount)
+                    totalUsers.setName("Total players: " + userCount)
                         .then(newChannel => console.log(`Stat channel renamed to: ${newChannel.name}`))
                         .catch(console.error);
                 }
