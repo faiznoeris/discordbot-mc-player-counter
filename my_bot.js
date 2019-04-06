13645b9488d36d7582092ccf7600f4dbfa90d11d
faiznoeris <m.faiznoeris@gmail.com>
2019-04-06 14:37:27 +0700

    fix

--- my_bot.js
+++ my_bot.js
@@ -11,13 +11,13 @@ bot.on("ready", async () => {
     const totalUsers = bot.channels.get('563975579032879105');
 
     setInterval(function() {
-        console.log('Getting stats update..')
+        // console.log('Getting stats update..')
         var mcIP = '139.99.70.135';
         var mcPort = 25574;
         var userCount = 0;
 
         var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
-        request(url, function(err, response, body) {
+        return request(url, function(err, response, body) {
             if(err) {
                 console.log(err);
                 return message.reply('Error getting Minecraft server status...');
@@ -26,11 +26,17 @@ bot.on("ready", async () => {
             if(body.online) {
                 if(body.players.now) {
                     userCount = body.players.now;
+                    console.log(body);
+                    console.log(userCount);
                     totalUsers.setName("Total Players: " + userCount)
                         .then(newChannel => console.log(`Stat channel renamed to: ${newChannel.name}`))
                         .catch(console.error);
                 }
+            } else {
+                totalUsers.setName("OFFLINE")
+                    .then(newChannel => console.log(`Stat channel renamed to: ${newChannel.name}`))
+                    .catch(console.error);
             }
         });
-    });
+    }, 5000);
 });
