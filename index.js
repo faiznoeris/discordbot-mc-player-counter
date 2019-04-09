8c48b26d354d2b1798e7552a7a452e9714a86761
faiznoeris <m.faiznoeris@gmail.com>
2019-04-09 17:34:24 +0700

    cleanup

--- index.js
+++ index.js
@@ -2,48 +2,49 @@ const Discord = require('discord.js');
 const bot = new Discord.Client({disableEveryone: true});
 const request = require('request');
 
-bot.login(process.env.token);
+const botToken = process.env.token;
+const serverIp = process.env.serverIp;
+const serverPort = process.env.serverport;
 
-bot.on("ready", async () => {
-    console.log(`${bot.user.username} has logged on.`)
+bot.login(token);
+
+bot.on('ready', async () => {
+    console.log(`${bot.user.username} has logged on.`);
 
     const guild = bot.guilds.get('310446682900791297');
-    const totalUsers = bot.channels.get('563975579032879105');
-    const totalDiscordUsers = bot.channels.get('564009370850164737');
+    const channelTotalPlayers = bot.channels.get('563975579032879105');
+    const channelTotalMembers = bot.channels.get('564009370850164737');
 
     setInterval(function() {
-        // console.log('Getting stats update..')
-        var userCount = 0;
+        const members = guild.memberCount;
+        const baseMcApiUrl = 'http://mcapi.us/server/status';
+        let players = 0;
+        let url = `${baseMcApiUrl}?ip=${serverIp}&port=${serverPort}`;
 
-        const discordUsers = guild.memberCount;
-        totalDiscordUsers.setName("Total members: " + discordUsers)
-            // .then(newChannel => console.log(`Stat channel renamed to: ${newChannel.name}`))
-            .catch(console.error);
-
-        let url = 'http://mcapi.us/server/status?ip=' + process.env.serverip + '&port=' + process.env.serverport;
-        if (!process.env.serverport) {
-            url = 'http://mcapi.us/server/status?ip=' + process.env.serverip;
+        if (!serverPort) {
+            url = `${baseMcApiUrl}?ip=${serverIp}`;
         }
 
+        channelTotalMembers.setName('Total members: ' + members)
+            .catch(console.error);
+
         request(url, function(err, response, body) {
+            const parsedBody = JSON.parse(body);
+
             if(err) {
                 console.log(err);
-                return message.reply('Error getting Minecraft server status...');
             }
-            body = JSON.parse(body);
-            console.log(body);
-            if(body.online) {
-                if(body.players) {
-                    userCount = body.players.now;
-                    totalUsers.setName("Total players: " + userCount)
-                        .then(newChannel => console.log(`Stat channel renamed to: ${newChannel.name}`))
+
+            if(parsedBody.online) {
+                if(parsedBody.players) {
+                    players = parsedBody.players.now;
+                    channelTotalPlayers.setName('Total players: ' + players)
                         .catch(console.error);
                 }
             } else {
-                totalUsers.setName("OFFLINE")
-                    .then(newChannel => console.log(`Stat channel renamed to: ${newChannel.name}`))
+                channelTotalPlayers.setName('OFFLINE')
                     .catch(console.error);
             }
         });
-    }, 30000);
+    }, 30000); //30sec
 });
